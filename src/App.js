a733e6f019a68e3569df2f4ccd530aad500fdee7
Runner <841766635@qq.com>
2018-06-27 17:03:09 +0800

    基本

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">react</div>
+    );
+  }
+}
+
+export default App;
