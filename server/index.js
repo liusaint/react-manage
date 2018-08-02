547d29272ddcedc08812f1e3d0c4717b13eb5755
Runner <841766635@qq.com>
2018-08-02 16:22:19 +0800

    一个普通的列表页面
    未没使用redux的版本。
    问题：
    1.setState后要使用改变后的数据是在setTimeout里面操作吗？
    2.搜索的参数与输入框的参数是不是不应该放在同一个数据里。

--- server/index.js
+++ server/index.js
@@ -79,3 +79,49 @@ app.get('/home', function(req, res, next){
   }
   res.send(resData);
 });
+
+
+app.get('/notice-list', function(req, res, next){
+
+  var resData = {
+    status: 1,
+    data: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+  }
+  setTimeout(()=>{
+    res.send(resData);
+  },1000)
+  
+});
+
--- src/views/ucenter/notice.js
+++ src/views/ucenter/notice.js
@@ -13,7 +13,8 @@ import {
 	Input,
 	Button,
 	Table,
-	Pagination
+	Pagination,
+	Loading
 } from 'element-react'
 
 
@@ -21,6 +22,9 @@ class Notice extends Component {
 	constructor() {
 		super();
 		this.state = {
+			searchData:{
+
+			},
 			columns: [{
 				label: "日期",
 				prop: "date",
@@ -33,63 +37,100 @@ class Notice extends Component {
 				label: "地址",
 				prop: "address"
 			}],
-			data: [{
-				date: '2016-05-02',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1518 弄'
-			}, {
-				date: '2016-05-04',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1517 弄'
-			}, {
-				date: '2016-05-01',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1519 弄'
-			}, {
-				date: '2016-05-03',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1516 弄'
-			}, {
-				date: '2016-05-02',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1518 弄'
-			}, {
-				date: '2016-05-04',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1517 弄'
-			}, {
-				date: '2016-05-01',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1519 弄'
-			}, {
-				date: '2016-05-03',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1516 弄'
-			}]
+			data: [],
+			page:1,
+			pageSize:10,
+			loading:false
 		}
 	}
-	componentWillMount() {}
+	componentWillMount() {
+		this.getList();
+	}
+	getList() {
+		this.setState({
+			loading: true
+		})
+		$.ajax({
+				url: 'http://localhost:3001/notice-list',
+				data: {
+					...this.state.searchData,
+					page: this.state.page,
+					pageSize: this.state.pageSize
+				},
+				dataType: 'json',
+			})
+			.done((data) => {
+				if (1 == data.status) {
+					this.setState({
+						data: data.data
+					})
+					this.setState({
+						loading: false
+					})
+				}
+			})
+	}
+	handleSearch(val){
+		
+		this.setState({
+			searchData:{
+				...this.searchData,
+				content:val
+			}
+		})
+	}
+	search(){
+		
+		this.getList();
+	}
+	reset(){
+		this.setState({
+			page:1,
+			searchData:{}
+		})
+		setTimeout(()=>{this.getList()});
+
+	}
+	pageChange(page){
+		this.setState({
+			page:page
+		})
+		setTimeout(()=>{this.getList()});
+	}
+	pageSizeChange(pageSize) {
+		this.setState({
+			pageSize: pageSize,
+			page:1
+		})
+		setTimeout(() => {
+			this.getList()
+		});
+	}
 	render() {
 		var pageList = this.props.pageList;
 		return (
-			<div> 
+			<div className="notice-page"> 
 			<div className="page-title">公告通知</div>
 			<div className="top-search-wrap">
-			公告信息<Input placeholder="请输入内容" />
-			公告类型<Input placeholder="请输入内容" />
-			发布日期<Input placeholder="请输入内容" />
-			<Button type="primary">查询</Button>
-			<Button>重置</Button>
+			<div className="search-part">公告信息<Input placeholder="请输入内容" onChange={this.handleSearch.bind(this)} value={this.state.searchData.content} /></div>
+			<div className="search-part">公告类型<Input placeholder="请输入内容" /></div>
+			<div className="search-part">发布日期<Input placeholder="请输入内容" /></div>
+			<Button type="primary" onClick={this.search.bind(this)}>查询</Button>
+			<Button onClick={this.reset.bind(this)}>重置</Button>
 			</div>
+			<Loading loading={this.state.loading}>
 			<Table
-      style={{
-        width: '100%'
-      }}
-      columns={this.state.columns}
-      maxHeight={200}
-      data={this.state.data}
-      />
-			<Pagination layout="prev, pager, next" total={50}/>
+			style={{
+				width: '100%'
+			}}
+			className="mt20"
+			columns={this.state.columns}
+			maxHeight={200}
+			data={this.state.data}
+			/>
+			</Loading>
+            <Pagination className="mt20" onSizeChange={this.pageSizeChange.bind(this)} onCurrentChange={this.pageChange.bind(this)}   layout="total, sizes, prev, pager, next, jumper" total={400} pageSizes={[10, 20, 50, 100]} pageSize={this.state.pageSize} currentPage={this.state.page}/>
+
 			</div>
 		)
 	}
